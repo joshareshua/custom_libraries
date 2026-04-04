00a88c0d568200a943e5bf1d6421fffdb749af2a
Josh <josh.hua10@gmail.com>
2026-04-04 23:59:10 +1100

    setup: Setup copy and move constructors/assignment operators for implementation

--- myVector.cpp
+++ myVector.cpp
@@ -7,6 +7,11 @@ class MyVector{
     int* data;
     size_t size;
     size_t capacity;
+
+    MyVector(MyVector& other){}
+    MyVector(MyVector& other){}
+    MyVector& operator=(MyVector& other){}
+    MyVector& operator=(MyVector& other){}
 };
 
 int main(){
