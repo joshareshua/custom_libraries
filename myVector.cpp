2ab3d9d52b90d7e8199d92d7354109c71de7edfd
Josh <josh.hua10@gmail.com>
2026-04-05 13:16:54 +1000

    feat: Implement MyVector copy constructor and assignment

--- myVector.cpp
+++ myVector.cpp
@@ -8,10 +8,49 @@ class MyVector{
     size_t size;
     size_t capacity;
 
-    MyVector(MyVector& other){}
-    MyVector(MyVector& other){}
-    MyVector& operator=(MyVector& other){}
-    MyVector& operator=(MyVector& other){}
+public:
+    ~MyVector(){
+        delete[] data;
+    }
+    
+    // Copy constructor for MyVector a = b where a = *this and b = other;
+    MyVector(MyVector& other){
+        capacity = other.capacity;
+        size = other.size;
+        int *data = new int[capacity];
+        for (size_t i{}; i < size; ++i){
+            data[i] = other.data[i];
+        }
+    }
+
+    // Copy assignment or a = b where a = *this and b = other;
+    MyVector& operator=(MyVector& other){
+        if (other.capacity <= capacity){
+            for (size_t i{}; i < other.size; ++i){
+                data[i] = other.data[i];
+            }
+
+        } else{
+            
+            int* newData = new int[other.capacity];
+
+            for (size_t i{}; i < other.size; ++i){
+                newData[i] = other.data[i];
+            }
+            delete[] data;
+            data = newData;
+            capacity = other.capacity;
+        }
+
+        size = other.size; 
+        return *this;
+    }
+
+    // Move constructor and assignment
+    MyVector(MyVector&& other){}    
+    MyVector& operator=(MyVector&& other){}
+
+
 };
 
 int main(){
