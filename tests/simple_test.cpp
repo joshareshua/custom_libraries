7013c96d721b7b05c4c2c3cbd226a8f6f906f2a0
Josh <josh.hua10@gmail.com>
2026-04-14 21:38:06 +1000

    test: Created simple test for copy constructor. Passes successfuly

--- tests/simple_test.cpp
+++ tests/simple_test.cpp
@@ -1,9 +1,23 @@
 #include "../bits/stdc++.h"
+#include "../myVector.cpp"
 
 using namespace std;
 
-int main(){
+bool simple_copy(){
+    MyVector a = {1,2,3,4,5};
+    MyVector b = a;
+    size_t sizeA = a.getSize();
+    int* dataA = a.getData(), *dataB = b.getData();
+    for (size_t i{}; i < sizeA; ++i){
+        if (dataA[i] != dataB[i]) return false;
+    }
+    return true;
 
+}
+
+int main(){
+    if (simple_copy()) cout << "Simple copy test passed!\n";
+    else if (!simple_copy()) cout << "Simple copy test failed\n";
 
     return 0;
 }
