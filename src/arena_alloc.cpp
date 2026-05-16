339adcee929991247e450ada48bcbc28496d1146
Josh <josh.hua10@gmail.com>
2026-05-16 22:36:00 +1000

    setup: Enable move constructor and assignment

--- src/arena_alloc.cpp
+++ src/arena_alloc.cpp
@@ -6,8 +6,12 @@ class ArenaAlloc{
 public:
 
     explicit ArenaAlloc(size_t chunk_size) : default_chunk_size(chunk_size){}
+
     ArenaAlloc(const ArenaAlloc&) = delete;
     ArenaAlloc& operator=(const ArenaAlloc&) = delete;
+    
+    ArenaAlloc(ArenaAlloc&&) noexcept = default;
+    ArenaAlloc& operator=(ArenaAlloc&&) noexcept = default;
 
     void* allocate(){}
 
