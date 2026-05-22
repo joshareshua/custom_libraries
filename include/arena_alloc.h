3de5945729a8530c014470079c1896c5dc6c12ec
Josh <josh.hua10@gmail.com>
2026-05-22 16:27:49 +1000

    refactor: Move constructor declarationsn to header file

--- include/arena_alloc.h
+++ include/arena_alloc.h
@@ -8,6 +8,12 @@ public:
 
     explicit ArenaAlloc(size_t chunk_size);
 
+    ArenaAlloc(const ArenaAlloc&) = delete;
+    ArenaAlloc& operator=(const ArenaAlloc&) = delete;
+
+    ArenaAlloc(ArenaAlloc&&) noexcept = default;
+    ArenaAlloc& operator=(ArenaAlloc&&) noexcept = default;
+
 private:
 };
 
--- src/arena_alloc.cpp
+++ src/arena_alloc.cpp
@@ -7,11 +7,6 @@ public:
 
     explicit ArenaAlloc(size_t chunk_size) : default_chunk_size(chunk_size){}
 
-    ArenaAlloc(const ArenaAlloc&) = delete;
-    ArenaAlloc& operator=(const ArenaAlloc&) = delete;
-    
-    ArenaAlloc(ArenaAlloc&&) noexcept = default;
-    ArenaAlloc& operator=(ArenaAlloc&&) noexcept = default;
 
     void* allocate(){}
 
