ec6bc0411884b0ecaa5b6cc15fac267e16f9afb2
Josh <josh.hua10@gmail.com>
2026-06-01 21:10:18 +1000

    setup: Add function signature for allocate() function to arena_alloc.cpp source file

--- src/arena_alloc.cpp
+++ src/arena_alloc.cpp
@@ -1,30 +1,8 @@
 #include "../bits/stdc++.h"
+#include "../include/arena_alloc.h"
 
 using namespace std;
 
-class ArenaAlloc{
-public:
+void* ArenaAlloc::allocate(std::size_t bytes, 
+        std::size_t alignment = alignof(std::max_align_t)){}
 
-    explicit ArenaAlloc(size_t chunk_size) : default_chunk_size(chunk_size){}
-
-
-    void* allocate(){}
-
-    void reset(){}
-
-    size_t used() const {};
-    size_t capacity() const{};
-    size_t chunk_count() const{};
-
-
-private:
-    struct Chunk{
-        unique_ptr<byte[]> data;
-        size_t size;
-        size_t offset;
-    };
-    vector<Chunk> chunks;
-    size_t default_chunk_size;
-    Chunk& add_chunk(std::size_t minimum_size);
-
-};
