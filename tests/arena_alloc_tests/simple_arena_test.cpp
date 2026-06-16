e39e963302c2f199f5d63dc87f69216aae8631aa
Josh <josh.hua10@gmail.com>
2026-06-17 09:57:31 +1000

    test: Implement simple initalize arena allocator and allocate bytes test

--- tests/arena_alloc_tests/simple_arena_test.cpp
+++ tests/arena_alloc_tests/simple_arena_test.cpp
@@ -5,6 +5,14 @@ using namespace std;
 
 
 
+void test_init(){
+    // testing initalizing allocator and allocating bytes
+    ArenaAlloc A(4);
+    A.allocate(100);
+    assert(A.capacity() == 100/4);
+
+}
+
 int main(){
     return 0;
 }
